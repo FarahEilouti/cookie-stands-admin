c1d51961fdba06b986bfae6a39c055192eb4c43a
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2023-01-15 13:55:27 +0200

    lab35

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,19 @@
 "use client";
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 import Form from './components/form'
-
+import LoginForm from './components/LoginForm';
+import {useContext} from 'react';
+import { AuthContext } from './contexts/auth';
 const inter = Inter({ subsets: ['latin'] })
 
 // starter code
 export default function Home() {
+  const {tokens} = useContext(AuthContext);
   return (
-    <div>
-     <Form/> 
-    </div>
+     <>
+    {tokens ? <> <Form /></> : <LoginForm />}
+    </>
+    
+    
   )
 }
