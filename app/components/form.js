2aacb2a1702656fca89aada46675751f863e893f
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2023-01-15 13:11:30 +0200

    lab33

--- app/components/form.js
+++ app/components/form.js
@@ -1,36 +1,33 @@
 import React from "react";
 import { useState } from "react";
-
-
+import ReportTable from "./reportTable";
 
 export default function Form() {
-    const [userInput, setUserInput] = useState({
-        location: "",
-        max: 0,
-        min: 0,
-        avg: 0,
-   })
-   const [fullReplyState,setFullReplyState] = useState([])
+  const [userInput, setUserInput] = useState({
+    location: "",
+    max: 0,
+    min: 0,
+    avg: 0,
+  });
+  const [fullReplyState, setFullReplyState] = useState([]);
 
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserInput((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
 
-     const handleChange = (e) => {
-          const name = e.target.name;
-          const value = e.target.value;
-          setUserInput((prev) => {
-               return { ...prev, [name]: value}
-          })
-}    
-     
-
-     const submitHandler = (e) => {
-          e.preventDefault();
-          setFullReplyState([...fullReplyState,userInput])
-     }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setFullReplyState([...fullReplyState, userInput]);
+  };
 
   return (
     <div>
       <section className="py-4 px-4 rounded-md text-center font-bold- text-[#1C82AD] ">
-        <form className="m-6" onSubmit={submitHandler}> 
+        <form className="m-6" onSubmit={submitHandler}>
           <label className="block font-bold text-2xl mb-2 ">
             {" "}
             Cookies Shop{" "}
@@ -86,16 +83,9 @@ export default function Form() {
       </section>
       <section className="w-full pt-10 text-center "></section>
 
-      <section className="w-full pt-10 text-center "></section>
-
-        {fullReplyState.length > 0 &&
-          fullReplyState.map((item, index) => {
-            return (
-              <div>
-                <p>{JSON.stringify(item)}</p>
-              </div>
-            );
-          })}
+      <section>
+        {fullReplyState.length > 0 && <ReportTable report={fullReplyState} />}
+        {fullReplyState.length == 0 && <h2>No Cookie Stands Available</h2>}
       </section>
     </div>
   );
